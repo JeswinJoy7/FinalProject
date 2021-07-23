ca9d955339b9e8977b842dfce4bebebe89932235
JeswinJoy7 <joy00032@algonquinlive.com>
2021-07-23 14:44:49 -0400

    Milestone 1 Commit

---
+++ app/src/main/java/algonquin/cst2335/finalproject/BusRouteDetails.java
@@ -0,0 +1,14 @@
+package algonquin.cst2335.finalproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BusRouteDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.busroute_details);
+    }
+}
